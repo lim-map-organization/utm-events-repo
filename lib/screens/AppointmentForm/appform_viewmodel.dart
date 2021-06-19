f1123285e772c1fe425cb4974354d5028a21547a
XDMAN1003 <63649862+XDMAN1003@users.noreply.github.com>
2021-06-19 23:52:22 +0800

    Delete

--- lib/screens/AppointmentForm/appform_viewmodel.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mapproject/screens/viewmodel.dart';
-
-class AppFormViewModel extends Viewmodel {
-  
-}
--- utm-event-json/android/app/src/main/kotlin/com/example/baoren_workplace/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.baoren_workplace
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
